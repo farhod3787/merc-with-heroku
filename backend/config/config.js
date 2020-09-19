5d3e01de5f2fc461330cde2efc1d9191a9b22f59
Bexruz Xolmuminov <bexruzxolmuminov@gmail.com>
2020-09-19 23:19:53 +0500

    clickuz

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,7 +3,18 @@ url = 'https://merc-with-paycom.herokuapp.com';
 
 // database = 'mongodb://localhost:27017/mercedec';
 database = 'mongodb+srv://farhod:7Q8SfcHx.F2J.HG@cluster0-uf7cc.mongodb.net/mercedec?retryWrites=true';
+
+//Click billing parametrlari
+service_id = 16067;
+merchant_id = 11547;
+secret_key = '4Yio3zkuxdEcrw';
+merchant_user_id = 16670;
+
 module.exports = {
   url,
-  database
+  database,
+  service_id,
+  merchant_id,
+  secret_key,
+  merchant_user_id
 }
